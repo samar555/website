4cbce3efc077771665e29800c6dbfb0727584554
samar555 <81468859+samar555@users.noreply.github.com>
2022-05-03 16:25:08 -0600

    Add files via upload

---
+++ portfolio website/js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","java developer","Web Developer","spring boot"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
