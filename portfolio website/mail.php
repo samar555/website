3f3259fc1cf51d0b385de09074c3075af124474e
samar555 <sachinmishraravi@gmail.com>
2025-04-29 13:16:36 +0530

    updated portfolio

--- portfolio website/mail.php
+++ portfolio website/mail.php
@@ -9,7 +9,7 @@ $to = "sachin.mishraravi@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message . "\r\n Subject =" . $subject;
 $headers = "From: noreply@portfolio.com" . "\r\n" .
-"CC: sachinmishra971188pan@gmail.com";
+"CC: sachinmishra9218@gmail.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
